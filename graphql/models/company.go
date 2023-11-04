7a7c29e67ee64fa7b1e083095d5b1d8b83594d1e
satyamsingh <satyam18577@gmail.com>
2023-11-04 17:53:50 +0530

    add

--- graphql/models/company.go
+++ graphql/models/company.go
@@ -106,3 +106,18 @@ func (s *Conn) FindJobByCompanyID(companyID string) ([]Job, error) {
 	}
 	return job, nil
 }
+func (s *Conn) AutoMigrate() error {
+
+	err := s.db.Migrator().AutoMigrate(&NewUser{}, &NewCompany{}, &NewJob{})
+	if err != nil {
+		return err
+	}
+
+	// AutoMigrate function will ONLY create tables, missing columns and missing indexes, and WON'T change existing column's type or delete unused columns
+	err = s.db.Migrator().AutoMigrate(&NewUser{}, &NewCompany{}, &NewJob{})
+	if err != nil {
+		// If there is an error while migrating, log the error message and stop the program
+		return err
+	}
+	return nil
+}
